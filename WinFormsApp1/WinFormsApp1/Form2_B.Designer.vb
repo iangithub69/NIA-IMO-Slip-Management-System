fb666273fd7934de782342d0b6a16d61bb1c6dbe
ian <emailaddnisya@gmail.com>
2024-02-26 16:42:28 +0800

    Update Form2_B.Designer.vb

--- WinFormsApp1/WinFormsApp1/Form2_B.Designer.vb
+++ WinFormsApp1/WinFormsApp1/Form2_B.Designer.vb
@@ -24,6 +24,15 @@ Partial Class Form2_B
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2_B))
         PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
@@ -37,11 +46,110 @@ Partial Class Form2_B
         PictureBox1.TabIndex = 37
         PictureBox1.TabStop = False
         ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(1548, 140)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(90, 45)
+        Label1.TabIndex = 38
+        Label1.Text = "Date"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(1518, 234)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(142, 45)
+        Label2.TabIndex = 39
+        Label2.Text = "Amount"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(303, 336)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(122, 45)
+        Label3.TabIndex = 40
+        Label3.Text = "Pay_To"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(303, 419)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(104, 45)
+        Label4.TabIndex = 41
+        Label4.Text = "Pesos"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(889, 571)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(226, 45)
+        Label5.TabIndex = 42
+        Label5.Text = "Cashier Name"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(888, 642)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(227, 45)
+        Label6.TabIndex = 43
+        Label6.Text = "Desgination 1"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(1351, 531)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(219, 45)
+        Label7.TabIndex = 44
+        Label7.Text = "Officer Name"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(1351, 610)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(227, 45)
+        Label8.TabIndex = 45
+        Label8.Text = "Designation 2"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label9.Location = New Point(1396, 695)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(124, 45)
+        Label9.TabIndex = 46
+        Label9.Text = "Branch"
+        ' 
         ' Form2_B
         ' 
         AutoScaleDimensions = New SizeF(13F, 32F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1934, 827)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
         Controls.Add(PictureBox1)
         FormBorderStyle = FormBorderStyle.None
         Name = "Form2_B"
@@ -49,7 +157,17 @@ Partial Class Form2_B
         Text = "Form2_B"
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
 End Class
