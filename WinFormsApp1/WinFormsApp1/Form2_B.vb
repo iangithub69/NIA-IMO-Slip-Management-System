d5a6f5399eec22429fec43895bf3553bd4cd0498
ian <emailaddnisya@gmail.com>
2024-02-26 16:42:35 +0800

    Update Form2_B.vb

--- WinFormsApp1/WinFormsApp1/Form2_B.vb
+++ WinFormsApp1/WinFormsApp1/Form2_B.vb
@@ -1,6 +1,92 @@
 ﻿Public Class Form2_B
+
+    Public Property Label1Text As String
+        Get
+            Return Label1.Text
+        End Get
+        Set(value As String)
+            Label1.Text = value
+        End Set
+    End Property
+
+    Public Property Label2Text As String
+        Get
+            Return Label2.Text
+        End Get
+        Set(value As String)
+            Label2.Text = value
+        End Set
+    End Property
+
+    Public Property Label3Text As String
+        Get
+            Return Label3.Text
+        End Get
+        Set(value As String)
+            Label3.Text = value
+        End Set
+    End Property
+
+    Public Property Label4Text As String
+        Get
+            Return Label4.Text
+        End Get
+        Set(value As String)
+            Label4.Text = value
+        End Set
+    End Property
+
+    Public Property Label5Text As String
+        Get
+            Return Label5.Text
+        End Get
+        Set(value As String)
+            Label5.Text = value
+        End Set
+    End Property
+
+    Public Property Label6Text As String
+        Get
+            Return Label6.Text
+        End Get
+        Set(value As String)
+            Label6.Text = value
+        End Set
+    End Property
+
+    Public Property Label7Text As String
+        Get
+            Return Label7.Text
+        End Get
+        Set(value As String)
+            Label7.Text = value
+        End Set
+    End Property
+
+    Public Property Label8Text As String
+        Get
+            Return Label8.Text
+        End Get
+        Set(value As String)
+            Label8.Text = value
+        End Set
+    End Property
+
+    Public Property Label9Text As String
+        Get
+            Return Label9.Text
+        End Get
+        Set(value As String)
+            Label9.Text = value
+        End Set
+    End Property
+
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
         Me.Close()
+
+    End Sub
+
+    Private Sub Form2_B_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
