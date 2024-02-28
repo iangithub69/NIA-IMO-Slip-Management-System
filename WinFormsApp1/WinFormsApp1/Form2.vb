bde71a552036b45a9d4396244b45ab8d5aebd403
ian <emailaddnisya@gmail.com>
2024-02-28 16:12:43 +0800

    Reverted
    28/02/24 - In good condition, this is before database insertion.

--- WinFormsApp1/WinFormsApp1/Form2.vb
+++ WinFormsApp1/WinFormsApp1/Form2.vb
@@ -342,4 +342,8 @@ Public Class Form2
     Private Sub TextBox8_TextChanged(sender As Object, e As EventArgs) Handles TextBox8.TextChanged
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
 End Class
